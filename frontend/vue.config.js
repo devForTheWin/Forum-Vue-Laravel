9e1d9733f474d5b506db135e26da2c085a844320
ArbuzTatiana <work.arbuztatiana@gmail.com>
2021-11-28 17:12:46 +0200

    dockerize app

---
+++ frontend/vue.config.js
@@ -0,0 +1,23 @@
+// module.exports = {
+//   devServer: {
+//     proxy: 'http://localhost:8000'
+//   }
+// }
+
+// vue.config.js
+module.exports = {
+    // options...
+    devServer: {
+        disableHostCheck: true,
+        host: 'localhost',
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+        },
+        watchOptions: {
+            poll: true
+        },
+        proxy: 'http://localhost/api',
+    }
+}
+
